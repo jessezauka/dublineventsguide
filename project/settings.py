9b4ac723f87d4c5c2539547f386476e006c2ff55
Jesse Zauka <jesse@liveradio.ie>
2020-07-28 19:15:15 +0100

    Whitenoise package added

--- project/settings.py
+++ project/settings.py
@@ -31,12 +31,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
-ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'dublineventsguide.herokuapp.com'
-]
+ALLOWED_HOSTS = ['dublineventsguide.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
