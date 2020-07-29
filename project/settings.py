fd584db50753072c0de53693189853ff9ac79d20
Jesse Zauka <jesse@liveradio.ie>
2020-07-29 07:26:50 +0100

    Alowed hosts update

--- project/settings.py
+++ project/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dublineventsguide.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME', '127.0.0.1')]
 
 # Application definition
 
